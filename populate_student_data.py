1fbfe64327588f084f179b5c0f39bff2d951bc4a
IND-PUN-MAC-125 <Vuclip@Vuclip.local>
2017-04-15 23:00:25 +0530

    1. Student database added 2. Some data added into the database

---
+++ populate_student_data.py
@@ -0,0 +1,65 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from database_setup import Student,Base
+
+engine = create_engine('sqlite:///student.db')
+
+# Bind the engine to the metadata of the Base class so that the
+# declaratives can be accessed through a DBSession instance
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+# A DBSession() instance establishes all conversations with the database
+# and represents a "staging zone" for all the objects loaded into the
+# database session object. Any change made against the objects in the
+# session won't be persisted into the database until you call
+# session.commit(). If you're not happy about the changes, you can
+# revert all of them back to the last commit by calling
+# session.rollback()
+session = DBSession()
+
+
+def add_into_db(student):
+    session.add(student)
+    session.commit()
+
+
+student = Student(name="Banty Kumar", address="401 Manas Dreams", city="Ranchi", stream="Computer", gender="Male")
+add_into_db(student)
+
+student = Student(name="Divyanish Bharindwal", address="Kothrud", city="Nashik", stream="Computer", gender="Male")
+add_into_db(student)
+
+student = Student(name="Ashutosh Bhadoria", address="Karve Road", city="Aurangabad", stream="Computer", gender="Male")
+add_into_db(student)
+
+student = Student(name="Indranil Bawane", address="Kothrud", city="Nagpur", stream="Computer", gender="Male")
+add_into_db(student)
+
+student = Student(name="Zoe", address="Bawdhan", city="Patna", stream="Computer", gender="Female")
+add_into_db(student)
+
+student = Student(name="Angela", address="SB Road", city="Pune", stream="Computer", gender="Female")
+add_into_db(student)
+
+student = Student(name="Michael", address="MIT Boys Hostel", city="Jaipur", stream="Mechanical", gender="Male")
+add_into_db(student)
+
+student = Student(name="John", address="MIT Boys Hostel", city="Latur", stream="Mechanical", gender="Male")
+add_into_db(student)
+
+student = Student(name="Lucy", address="4th Eve Avenue", city="Mumbai", stream="Mechanical", gender="Female")
+add_into_db(student)
+
+student = Student(name="Gus", address="2192 Baker Street", city="London", stream="Electrical", gender="Male")
+add_into_db(student)
+
+student = Student(name="Daisy", address="3430 16th Street", city="Delhi", stream="Civil", gender="Female")
+add_into_db(student)
+
+student = Student(name="Samatha", address="3430 16th Street", city="Delhi", stream="Civil", gender="Female")
+add_into_db(student)
+
+student = Student(name="Abby", address="1149 Chestnut St.", city="Kolkatta", stream="Infomation Technology", gender="Female")
+add_into_db(student)
