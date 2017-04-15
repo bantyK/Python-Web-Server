dda1aec522ac3291a6b47ffe94a403a1096e5ef8
IND-PUN-MAC-125 <Vuclip@Vuclip.local>
2017-04-16 01:52:27 +0530

    Update and delete requests are added.

--- database_operations.py
+++ database_operations.py
@@ -19,15 +19,40 @@ DBSession = sessionmaker(bind=engine)
 # session.rollback()
 session = DBSession()
 
+
 def get_all_students():
     return session.query(Student).all()
 
 
 def get_students_with_name(student_name):
-    return session.query(Student).filter_by(name = student_name).all()
+    return session.query(Student).filter_by(name=student_name).all()
 
 
 def add_into_database(new_student):
     session.add(new_student)
     session.commit()
     print "added into database"
+
+
+def updateStudentData(old_student_name, new_student):
+    student = session.query(Student).filter_by(name=old_student_name).one()
+
+    if new_student.name:
+        student.name = new_student.name
+    if new_student.address:
+        student.address = new_student.address
+    if new_student.city:
+        student.city = new_student.city
+    if new_student.stream:
+        student.stream = new_student.stream
+    if new_student.gender:
+        student.gender = new_student.gender
+
+    session.add(student)
+    session.commit()
+
+
+def deleteStudent(student_name):
+    student = session.query(Student).filter_by(name=student_name).one()
+    session.delete(student)
+    session.commit()
--- web_server.py
+++ web_server.py
@@ -14,16 +14,31 @@ class WebServerHandler(BaseHTTPRequestHandler):
                 output = self.get_view_for_student_list(students)
                 self.wfile.write(output)
                 return
-            if self.path.endswith("/"):
+            elif self.path.endswith("/"):
                 self.add_header()
                 student_name = self.path.split("/")[2]
                 students_with_name = db.get_students_with_name(student_name)
                 output = self.get_view_for_single_student(students_with_name)
                 self.wfile.write(output)
                 return
-            if self.path.endswith('/student/add'):
+            elif self.path.endswith('/student/add'):
                 self.add_header()
                 output = self.get_view_for_create_student()
+                self.wfile.write(output)
+                return
+            elif self.path.endswith('/edit'):
+                self.add_header()
+                student_name = self.path.split("/")[2]
+                print student_name
+                student = db.get_students_with_name(student_name)
+                output = self.get_view_for_edit(student[0])
+                self.wfile.write(output)
+                return
+            elif self.path.endswith("/delete"):
+                self.add_header()
+                student_name = self.path.split("/")[2]
+                student = db.get_students_with_name(student_name)
+                output = self.get_view_for_delete(student[0])
                 self.wfile.write(output)
                 return
 
@@ -48,6 +63,32 @@ class WebServerHandler(BaseHTTPRequestHandler):
 
                     self.add_redirect_headers()
 
+            if self.path.endswith("/edit"):
+                ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+                if ctype == 'multipart/form-data':
+                    fields = cgi.parse_multipart(self.rfile, pdict)
+
+                    student_name = fields.get("name")[0]
+                    student_address = fields.get("address")[0]
+                    student_city = fields.get("city")[0]
+                    student_stream = fields.get("stream")[0]
+                    student_gender = fields.get("gender")[0]
+
+                    print(student_name, student_address, student_city, student_stream, student_gender)
+                    new_student = Student(name=student_name, address=student_address, city=student_city,
+                                          stream=student_stream, gender=student_gender)
+
+                student_name = self.path.split("/")[2]
+                db.updateStudentData(student_name, new_student)
+                self.add_redirect_headers()
+
+            if self.path.endswith("/delete"):
+                student_name = self.path.split("/")[2]
+                db.deleteStudent(student_name)
+                self.add_redirect_headers()
+
+
+
         except Exception as e:
             print e
 
@@ -64,6 +105,7 @@ class WebServerHandler(BaseHTTPRequestHandler):
 
     def get_view_for_student_list(self, students):
         output = "<html><body>"
+        output += "<h1><a href = '/student/add'>Add a new student</a></h1>"
         for student in students:
             output += '<h1> <a href="/student/%s/"> %s </a></h1>' % (student.name, student.name)
         output += "</body></html>"
@@ -77,6 +119,9 @@ class WebServerHandler(BaseHTTPRequestHandler):
             output += '<p>City :  %s </p>' % student.city
             output += '<p>Stream :  %s </p>' % student.stream
             output += '<p>Gender :  %s </p>' % student.gender
+            output += '<p><a href="/student/%s/edit"> Edit  </a><a href="/student/%s/delete"> Delete</a></p>' % (
+                student.name, student.name)
+
         output += "</body></html>"
         return output
 
@@ -93,6 +138,28 @@ class WebServerHandler(BaseHTTPRequestHandler):
                         <p><input type='submit' name = 'Add'></p>
                     </form>
                   '''
+        output += "</body></html>"
+        return output
+
+    def get_view_for_edit(self, student):
+        output = "<html><body>"
+        output += "<form method='POST' enctype=multipart/form-data action = /student/%s/edit>" % student.name
+        output += "<p>Name  : <input type='text' name = 'name' placeholder = '%s'></p>" % student.name
+        output += "<p>Address : <input type='text' name = 'address' placeholder = '%s'></p>" % student.address
+        output += "<p>City : <input type='text' name = 'city' placeholder = '%s'></p>" % student.city
+        output += "<p>Stream : <input type='text' name = 'stream' placeholder = '%s'></p>" % student.stream
+        output += "<p>Gender : <input type='text' name = 'gender' placeholder = '%s'></p>" % student.gender
+        output += "<p><input type='submit' value = 'Update'></p>"
+        output += "</form>"
+        output += "</body></html>"
+        return output
+
+    def get_view_for_delete(self, student):
+        output = "<html><body>"
+        output += "<h1> Are you sure you want to delete %s from database?" % student.name
+        output += "<form method='POST' enctype=multipart/form-data action = /student/%s/delete>" % student.name
+        output += "<input type='submit' value = 'Delete'>"
+        output += "</form>"
         output += "</body></html>"
         return output
 
