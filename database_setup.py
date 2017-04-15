75fd1e254645ea55e2710e23e6721998b9c737b0
IND-PUN-MAC-125 <Vuclip@Vuclip.local>
2017-04-15 22:59:53 +0530

    1. Student database added 2. Some data added into the database

--- database_setup.py
+++ database_setup.py
@@ -1,1 +1,21 @@
-import sqlalchemy
+from sqlalchemy import Column, Integer, String, ForeignKey, Date, Numeric
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class Student(Base):
+    __tablename__ = 'student'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+    address = Column(String(80))
+    city = Column(String(20))
+    stream = Column(String(20))
+    gender = Column(String(7))
+
+
+engine = create_engine('sqlite:///student.db')
+Base.metadata.create_all(engine)
